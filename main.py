e984b373ec203d5435bf27778855d95a994ab024
Porramet Teang-orn <60021414@up.ac.th>
2022-02-22 13:01:29 +0700

    update

--- main.py
+++ main.py
@@ -51,6 +51,3 @@ async def change_active_number(number : int = Form(...)):
     mydb.commit()
     return RedirectResponse("/change",status_code=302)
 
-
-if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8000)
